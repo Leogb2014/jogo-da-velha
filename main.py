16c1db6211a5bbcb366fddc8cee741ad1d5c7ea8
Leonardo Gobetti <leonardogobetti@hotmail.com>
2026-03-01 15:54:13 -0300

    chore: configuração inicial e método interface

---
+++ main.py
@@ -0,0 +1,9 @@
+def interface():
+    print("   0   1   2")
+    print("0 [{}] [{}] [{}]".format(tabuleiro[0][0],tabuleiro[0][1],tabuleiro[0][2]))
+    print("1 [{}] [{}] [{}]".format(tabuleiro[1][0],tabuleiro[1][1],tabuleiro[1][2]))
+    print("2 [{}] [{}] [{}]".format(tabuleiro[2][0],tabuleiro[2][1],tabuleiro[2][2]))
+
+tabuleiro = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+
+interface()
